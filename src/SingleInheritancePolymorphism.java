c29c6944b01e2991701dab3e334d35dae368561f
Mohanasri <mohana05117@gmail.com>
2026-08-06 19:46:08 +0530

    lab program

--- src/SingleInheritancePolymorphism.java
+++ src/SingleInheritancePolymorphism.java
@@ -32,7 +32,7 @@ class B extends A {
 	}
 }
 
-public class SingleInheritance {
+public class SingleInheritancePolymorphism {
 	public static void main(String[] args) {
 		B obj = new B();
 		System.out.println("Variable : " + obj.x);
