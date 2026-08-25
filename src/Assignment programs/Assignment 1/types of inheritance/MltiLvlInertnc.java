4c253fc078737e3469014290909e397efe5c4e22
Mohanasri <mohana05117@gmail.com>
2026-08-25 20:03:54 +0530

    Implement multilevel inheritance example with Animal

--- src/Assignment programs/Assignment 1/types of inheritance/MltiLvlInertnc.java
+++ src/Assignment programs/Assignment 1/types of inheritance/MltiLvlInertnc.java
@@ -1,35 +1,49 @@
+// Parent class
 class Animal
 {
+    // Method of Animal class
     void eat()
     {
         System.out.println("Animal eats");
     }
 }
 
+// Dog inherits Animal
 class Dog extends Animal
 {
+    // Method of Dog class
     void bark()
     {
         System.out.println("Dog barks");
     }
 }
 
+// Puppy inherits Dog
+// Dog already inherits Animal
 class Puppy extends Dog
 {
+    // Method of Puppy class
     void play()
     {
         System.out.println("Puppy plays");
     }
 }
 
+// Main class
 class MultilevelInheritance
 {
     public static void main(String args[])
     {
+        // Creating an object of Puppy class
         Puppy p = new Puppy();
 
+        // Calling eat() method inherited from Animal
         p.eat();
+
+        // Calling bark() method inherited from Dog
         p.bark();
+
+        // Calling play() method of Puppy
         p.play();
     }
 }
