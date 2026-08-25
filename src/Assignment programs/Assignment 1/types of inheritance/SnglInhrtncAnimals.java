4e194e85a0e89536fb40d40b41107066657d62a5
Mohanasri <mohana05117@gmail.com>
2026-08-25 20:05:20 +0530

    Implement single inheritance example with Animal and Dog

--- src/Assignment programs/Assignment 1/types of inheritance/SnglInhrtncAnimals.java
+++ src/Assignment programs/Assignment 1/types of inheritance/SnglInhrtncAnimals.java
@@ -1,24 +1,35 @@
+// Parent class
 class Animal
 {
+    // Method of Animal class
     void eat()
     {
         System.out.println("Animal eats");
     }
 }
+
+// Child class inherits Animal class
 class Dog extends Animal
 {
+    // Method of Dog class
     void bark()
     {
         System.out.println("Dog barks");
     }
 }
+
+// Main class
 class SingleInheritance
 {
     public static void main(String args[])
     {
+        // Creating an object of Dog class
         Dog d = new Dog();
 
+        // Calling eat() method inherited from Animal class
         d.eat();
+
+        // Calling bark() method of Dog class
         d.bark();
     }
 }
