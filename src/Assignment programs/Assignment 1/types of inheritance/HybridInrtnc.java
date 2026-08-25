aa5fe77ff32aa6ae0c6b403c3323cdd295820fa3
Mohanasri <mohana05117@gmail.com>
2026-08-25 20:02:24 +0530

    Refactor HybridInrtnc.java for clarity and formatting

--- src/Assignment programs/Assignment 1/types of inheritance/HybridInrtnc.java
+++ src/Assignment programs/Assignment 1/types of inheritance/HybridInrtnc.java
@@ -1,42 +1,59 @@
-class Animal
+// Parent class
+class Animal 
 {
-    void eat()
+    // Method of Animal class
+    void eat() 
     {
         System.out.println("Animal eats");
     }
 }
 
-interface Pet
+// First interface
+interface Pet 
 {
+    // Abstract method
     void play();
 }
 
-interface Friendly
+// Second interface
+interface Friendly 
 {
+    // Abstract method
     void friendly();
 }
 
-class Dog extends Animal implements Pet, Friendly
+// Dog inherits Animal class
+// and implements both Pet and Friendly interfaces
+class Dog extends Animal implements Pet, Friendly 
 {
-    public void play()
+    // Implementing play() method of Pet interface
+    public void play() 
     {
         System.out.println("Dog plays");
     }
 
-    public void friendly()
+    // Implementing friendly() method of Friendly interface
+    public void friendly() 
     {
         System.out.println("Dog is friendly");
     }
 }
 
-class HybridInheritance
+// Main class
+class HybridInheritance 
 {
-    public static void main(String args[])
+    public static void main(String args[]) 
     {
+        // Creating an object of Dog class
         Dog d = new Dog();
 
+        // Calling eat() method inherited from Animal
         d.eat();
+
+        // Calling play() method implemented from Pet
         d.play();
+
+        // Calling friendly() method implemented from Friendly
         d.friendly();
     }
 }
